27ab6b0c861519ae651a2f2fbe4bcc69c75ecffd
Vedant Khairnar <vedant3khairnar@gmail.com>
2026-01-22 12:27:42 +0530

    Added the Apis Assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,27 @@ import { HomeComponent } from './home/home.component';
 import { TextDisplayComponent } from './text-display/text-display.component';
 import { FormattersComponent } from './formatters/formatters.component';
 import { FormsModule } from '@angular/forms';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ApisComponent } from './apis/apis.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     TextDisplayComponent,
-    FormattersComponent
+    FormattersComponent,
+    NavigationComponent,
+    ApisComponent
   ],
   imports: [  
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule ,
+    RouterModule
   ],
   // services
   providers: [],
